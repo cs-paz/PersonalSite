7dfb30b4d3f9689104ecf8f8d676236379ddd792
Christian Szablewski-Paz <christian.szablewski-paz@narmitech.com>
2023-11-30 15:05:51 -0500

    begin typography

--- app/components/HomeContent.tsx
+++ app/components/HomeContent.tsx
@@ -1,5 +1,6 @@
 import Grid from '@mui/joy/Grid';
 import Item from '@mui/joy/Grid';
+import Typography from '@mui/joy/Typography';
 import BioCard from './BioCard'
 import Box from '@mui/joy/Box';
 
@@ -22,18 +23,27 @@ function HomeContent() {
             <Grid xs={8}>
                 <Item>
                     <Box
-                        component="img"
                         sx={{
-                            zIndex: -1,
-                            height: "560px",
+                            paddingLeft: "20px",
                             width: "100%",
                             objectFit: 'cover',
-                            backgroundImage: 'url(/maine.png)',
                             borderRadius: "15px",
                             backgroundPosition: '60% 50%',
-                            boxShadow: "lg",
+                            boxShadow: "0px 5px 30px 0px rgba(0,0,0,0.75)",
                         }}
-                    />
+                    >
+                        <Typography 
+                            level="title-lg"
+                            sx = {{
+                                fontSize: "4rem",
+                                fontWeight: "200",
+                                color: "#000000",
+                                padding: "0px 0px 10px 0px",
+                            }}
+                        >
+                            How goes it?
+                        </Typography>
+                    </Box>
                 </Item>
             </Grid>
             <Grid xs={4} style={{height: "100%"}}>
