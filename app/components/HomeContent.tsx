fdaff179405271c57f321d621f6de60d3ef2887a
Christian Szablewski-Paz <christian.szablewski-paz@narmitech.com>
2023-12-05 21:26:46 -0500

    css fixes and introduce UnderGrid

--- app/components/HomeContent.tsx
+++ app/components/HomeContent.tsx
@@ -12,8 +12,7 @@ const main_container_style = {
 }
 
 const grid_style = {
-    height:"90vh",
-    minHeight:"600px",
+    height:"480px",
     width: "1250px",
     maxHeight: "800px",
     padding: "0px 20px 0px 20px",
@@ -32,32 +31,33 @@ const navbar_container_style = {
 
 const box_style = {
     padding: "0px 20px 0px 20px",
-    width: "730px",
+    width: "100%",
     objectFit: 'cover',
     borderRadius: "15px",
     backgroundPosition: '60% 50%',
     boxShadow: "0px 5px 30px 0px rgba(0,0,0,0.75)",
     height: "450px",
+    margin: "auto"
 }
 
 const text_container_style = {
-    padding: "0px 3% 20px 3%",
+    padding: "0px 0px 20px 3%",
     width: "100%",
     maxWidth: "700px",
 }
 
 const title_style = {
-    fontSize: "4.2rem",
+    fontSize: "5.0rem",
     fontWeight: "200",
     color: "#332f2c",
     borderBottom: "0.5px solid #332f2c",
     lineHeight: "1.2",
-    padding: "10px 0px 0px 0px",
+    padding: "15px 0px 0px 0px",
     marginBottom: "10px"
 }
 
 const main_text_style = {
-    fontSize: "1.67rem",
+    fontSize: "1.73rem",
     fontWeight: "200",
     color: "#332f2c",
     padding: "0px 0px 10px 0px",
@@ -65,6 +65,20 @@ const main_text_style = {
 
 const bio_card_container_style = {
     margin: "auto",
+}
+
+const under_grid_style = {
+    height:"150px",
+    width: "1250px",
+    padding: "0px 20px 0px 20px",
+    borderRadius: "15px",
+    margin: "auto",
+    color: "#332f2c",
+    backgroundColor: "black",
+}
+
+const italicized_style = {
+    fontStyle: 'italic',
 }
 
 const MainText = () => (
@@ -77,10 +91,47 @@ const MainText = () => (
                 My name is Christian. I am a full stack software engineer based out of the New York City area, and I am always looking for new opportunities to learn and grow as a developer.
             </Typography>
             <Typography sx={main_text_style}>
-                I am experienced in building web and mobile enterprise systems from front to back.  Solving real world problems using technology is my passion, and why not make it look pretty?
+                I am experienced in building web and mobile enterprise systems from front to back.  Solving real problems using technology is my passion, and building web apps is fun! 
+                <Box><span style={italicized_style}>And hey! Why not make them look good too?</span></Box>
             </Typography>
         </Box>
     </Box>
+)
+
+const MainGrid = () => (
+    <Grid
+        container
+        spacing={1}
+        style={grid_style}
+    >
+        <Grid xs={8}>
+            <Item>
+                <MainText/>
+            </Item>
+        </Grid>
+        <Grid xs={4}>
+            <Item sx={bio_card_container_style}>
+                <BioCard/>
+            </Item>
+        </Grid>
+    </Grid>
+)
+
+const UnderGrid = () => (
+    <Grid
+        container
+        spacing={2}
+        style={under_grid_style}
+    >
+        <Grid xs={7}>
+            <Item>
+            </Item>
+        </Grid>
+        <Grid xs={5}>
+            <Item>
+            </Item>
+        </Grid>
+    </Grid>
 )
 
 
@@ -88,22 +139,8 @@ const HomeContent = () => {
     return (
         <Box style={main_container_style}>
             <Grid style={navbar_container_style}><Box style={navbar_container_style}><NavBar/></Box></Grid>
-            <Grid
-                container
-                spacing={2}
-                style={grid_style}
-            >
-                <Grid xs={7}>
-                    <Item>
-                        <MainText/>
-                    </Item>
-                </Grid>
-                <Grid xs={5}>
-                    <Item sx={bio_card_container_style}>
-                        <BioCard/>
-                    </Item>
-                </Grid>
-            </Grid>
+            <MainGrid/>
+            <UnderGrid/>
         </Box>
     )
 }
